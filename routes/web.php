ee6397d28ac4a5af53e2dec193bfda74c0568975
= <bethaniafreitas@yahoo.com.br>
2022-08-10 20:57:48 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get('/login', function(){return "Login";})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'Rota acessada não existe. <a href="/">Clique aqui</a> para retornar ao menu inicial';
+});
+
