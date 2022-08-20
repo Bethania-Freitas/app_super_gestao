b8283cec97c56f4231eba9ebc4081b92187ca79f
= <bethaniafreitas@yahoo.com.br>
2022-08-19 22:56:07 -0300

    fatories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(FornecedorSeeder::class);
+        // $this->call(FornecedorSeeder::class);
         $this->call(SiteContatoSeeder::class);
     }
 }
--- database/seeders/SiteContatoSeeder.php
+++ database/seeders/SiteContatoSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\SiteContato;
+use Database\Factories\SiteContatoFactory;
+use Faker\Factory;
 
 class SiteContatoSeeder extends Seeder
 {
@@ -15,12 +17,14 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        SiteContato::create([
-            'nome' => 'João',
-            'telefone' =>'11-9999.9999',
-            'email' => 'joao@teste.com.br',
-            'motivo_contato' => 1,
-            'mensagem' => 'Gostaria de orçamento'
-        ]);
+        // SiteContato::create([
+        //     'nome' => 'João',
+        //     'telefone' =>'11-9999.9999',
+        //     'email' => 'joao@teste.com.br',
+        //     'motivo_contato' => 1,
+        //     'mensagem' => 'Gostaria de orçamento'
+        // ]);
+
+        SiteContato::factory()->count(100)->create();
     }
 }
