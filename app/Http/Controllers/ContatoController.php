a40116aa03c481e71488ba3d5c0cb553945a9148
= <bethaniafreitas@yahoo.com.br>
2022-09-02 16:10:18 -0300

    validacoes em portugues

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -18,13 +18,24 @@ class ContatoController extends Controller
     public function salvar(Request $request) {
 
         //realizar a validação dos dados do formulário recebidos no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40|unique:site_contatos',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000'
-        ]);
+
+        $regras = [
+                            'nome' => 'required|min:3|max:40|unique:site_contatos',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute é obrigatório',
+            'min' => 'O Campo :attribute precisa ter o mínimo de 3 caracteres',
+            'max' => 'O Campo :attribute ultrapassou o máximo de caracteres permitido',
+            'unique' => 'Nome já cadastrado no nosso sistema',
+            'email' => 'Campo email inválido',
+        ];
+        $request->validate($regras, $feedback);
+
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
