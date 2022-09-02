6d92ca7cb7dd56877676258c855d54a9e5d29b2b
= <bethaniafreitas@yahoo.com.br>
2022-09-02 15:49:06 -0300

    correcao validacoes

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,36 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\SiteContato;
+use App\models\SiteContato;
 use App\Models\MotivoContato;
-use App\Http\Requests\SiteContatoRequest;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request){
+    public function contato(Request $request) {
 
-        // $contato = new SiteContato();
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-        // $contato->save();
-
-        // $contato = new SiteContato();
-        // $contato->fill($request->all());
-        // $contato->save();
-        // print_r($contato->getAttributes());
-        
         $motivo_contatos = MotivoContato::all();
 
         return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
-    public function salvar(SiteContatoRequest $request){
-        
+    public function salvar(Request $request) {
+
+        //realizar a validação dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
         SiteContato::create($request->all());
         return redirect()->route('site.index');
-       
     }
 }
