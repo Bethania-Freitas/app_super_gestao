2be8a4dcefb655dc8514500ce8499626c6f285b9
= <bethaniafreitas@yahoo.com.br>
2022-09-08 17:10:02 -0300

    paginacao

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -43,7 +43,8 @@
                     @endforeach
                 </tbody>
             </table>
-
+            {{ $fornecedores->appends($request)->links() }}
+            
         </div>
     </div>
 
