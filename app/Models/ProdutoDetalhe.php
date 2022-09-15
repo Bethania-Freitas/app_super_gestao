9df0cc0aec009b6c9d34e5823c73b8fd85e379e5
= <bethaniafreitas@yahoo.com.br>
2022-09-15 16:37:36 -0300

    produtodetalhe

--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProdutoDetalhe extends Model
 {
+    protected $fillable = [
+        'produto_id',
+        'comprimento',
+        'largura',
+        'altura',
+        'unidade_id'
+    ];
+    
     use HasFactory;
 }
